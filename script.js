d8084202136bc0c95866187f1ed8da095663503e
Umesh Kumar <103483751+Umeshboby@users.noreply.github.com>
2024-01-30 15:42:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+//your JS code here. If required.
+  document.addEventListener('DOMContentLoaded', function () {
+            // DOM is loaded, add your code here
+            var body = document.body;
+            body.innerHTML = 'DOM load success';
+        });
